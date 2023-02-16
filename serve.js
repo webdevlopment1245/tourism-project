860d3aa063f4235c34a49221f9668bb6da018140
webdevlopment1245 <125587717+webdevlopment1245@users.noreply.github.com>
2023-02-17 00:54:39 +0530

    first

---
+++ serve.js
@@ -0,0 +1,29 @@
+// var http = require('http');
+// var fs = require('fs');
+// var path = require('path');
+
+// http.createServer(function(req, res){
+
+//     if(req.url === "/"){
+//         fs.readFile("./public/index.html", "UTF-8", function(err, html){
+//             res.writeHead(200, {"Content-Type": "text/html"});
+//             res.end(html);
+//         });
+//     }else if(req.url.match("\.css$")){
+//         var cssPath = path.join(__dirname, 'public', req.url);
+//         var fileStream = fs.createReadStream(cssPath, "UTF-8");
+//         res.writeHead(200, {"Content-Type": "text/css"});
+//         fileStream.pipe(res);
+
+//     }else if(req.url.match("\.jpg$")){
+//         var imagePath = path.join(__dirname, 'public', req.url);
+//         var fileStream = fs.createReadStream(imagePath);
+//         res.writeHead(200, {"Content-Type": "image/jpg"});
+//         fileStream.pipe(res);
+//     }else{
+//         res.writeHead(404, {"Content-Type": "text/html"});
+//         res.end("No Page Found");
+//     }
+
+// }).listen(3000);
+// console.log(" SERVER RUN SUCCESFULLY");
---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+var fs = require('fs');
+var path = require('path');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    if(req.url === "/"){
+        fs.readFile("./public/index.html", "UTF-8", function(err, html){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(html);
+        });
+    }else if(req.url.match("\.css$")){
+        var cssPath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(cssPath, "UTF-8");
+        res.writeHead(200, {"Content-Type": "text/css"});
+        fileStream.pipe(res);
+
+    }else if(req.url.match("\.png$")){
+        var imagePath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, {"Content-Type": "image/png"});
+        fileStream.pipe(res);
+    }else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("No Page Found");
+    }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
