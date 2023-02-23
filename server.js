42847d303709ea413da7cf28b3c37cb83e6cd327
webdevlopment1245 <125587717+webdevlopment1245@users.noreply.github.com>
2023-02-23 09:24:19 +0530

    ii

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, {"Content-Type": "text/html"});
             res.end(html);
         });
-    }else if(req.url.match("\.css$")){
+    }
+    else if(req.url.match("\.css$")){
         var cssPath = path.join(__dirname, 'public', req.url);
         var fileStream = fs.createReadStream(cssPath, "UTF-8");
         res.writeHead(200, {"Content-Type": "text/css"});
